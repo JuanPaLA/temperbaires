5d4b9eba5edfb2cbab1ecf05bbf31b9d4904a8f2
JuanPaLA <juanlopezalurraldemail.com>
2020-05-16 10:50:20 -0300

    se pudo arreglar la lectura de datos, se procede con el envio a DB

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,32 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios').default;
+const request = require('request');
 
 router.get('/test', (req, res) => {
-    res.send({
-        msg: 'cacoso'    })
+    
+    request('https://ws.smn.gob.ar/map_items/weather/', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+        //res.send(typeof(body))
+        var data = JSON.parse(body)
+        //res.send(data[0]) 
+        //res.send(typeof(data))    
+        for (index in data){
+            if(data[index].name === 'Aeroparque Buenos Aires'){
+                var baires = data[index]
+                
+                bs = {}
+                bs = baires.weather    
+                console.log(bs.humidity, bs.st, bs.temp)
+                res.send(bs)
+            }
+
+        }
+        
+        
+    }
+});
+
 })
 
 
@@ -17,11 +39,8 @@ router.get('/temp', (req, res) => {
     })
     .catch(err => next(err));
 
-    
-    
-
-
 });
+
 
 
 
